842fb411f2cfe005cb9ae7f4abda0133e716e144
azevedo28 <stheffanyazevedo28@gmail.com>
2025-08-22 20:15:53 -0300

    me estressando

--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
@@ -1,7 +1,10 @@
 
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
+    ImageView imgMenuPrincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +25,15 @@ public class MenuPrincipalActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        imgMenuPrincipal = findViewById(R.id.ImgMenuPrincipal);
+
+        imgMenuPrincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),
+                        ItensVendaActivity.class));
+            }
+        });
     }
+
 }
