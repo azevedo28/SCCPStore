baf20c4a00f2a1caf75c06544cf522d82e32c534
azevedo28 <stheffanyazevedo28@gmail.com>
2025-08-22 21:21:02 -0300

    me estressa

---
+++ app/src/main/java/br/com/etecia/myapp/AcessoriosFragment.java
@@ -0,0 +1,24 @@
+package br.com.etecia.myapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AcessoriosFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_acessorios, container, false);
+
+        //vc vai codificar aqui //
+
+
+        return view;
+    }
+}
